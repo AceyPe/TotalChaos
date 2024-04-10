17cd3b29f97e88eb76e780fe8373329f875ac632
Mohamed Taymour <mohamedtaymour1@gmail.com>
2024-04-10 14:23:48 +0200

    Discovered new issue with veichle + major issues with rotations and wheel, car subject to change

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,19 +6,33 @@ using UnityEngine.UI;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] Rigidbody rigidbody = new Rigidbody();
+    Rigidbody rigidbody;
+    Transform transform;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        if(Input.GetKey(KeyCode.W))
         {
-            rigidbody.AddForce(new Vector3(20, 20, 20), ForceMode.Impulse);
+            rigidbody.AddForce(new Vector3(20,0,0),ForceMode.Impulse);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            rigidbody.AddForce(new Vector3(-20,0,0),ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.AddTorque(new Vector3(0, 20, 0) * 0.2f, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddTorque(new Vector3(0, -20, 0) * 0.2f, ForceMode.Impulse);
         }
     }
 }
